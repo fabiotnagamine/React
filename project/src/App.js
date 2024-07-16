628dcfd9cf55c5b7ef551cc198b2a174d4645d4b
Fabio Nagamine <84332616+fabiotnagamine@users.noreply.github.com>
2024-07-16 07:59:13 -0300

    chore: Update project configuration files and dependencies

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,29 @@
-import './App.css';
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-function App(){
-  return <h1>Hello World</h1>;
+import store, { persistor } from './store';
+import history from './services/history';
+import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
+import Routes from './routes';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} className="toast-container" />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
