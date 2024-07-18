61d2541450b74da9887b00f7259f3e66450d7487
Fabio Nagamine <84332616+fabiotnagamine@users.noreply.github.com>
2024-07-18 17:27:47 -0300

    chore: Refactor Main component to handle task editing and deletion

--- forms_React/forms_react/src/components/Main.js
+++ forms_React/forms_react/src/components/Main.js
@@ -1,82 +1,95 @@
-import React, { Component } from "react";
-import "./Main.css";
-import { FaEdit, FaPlus, FaWindowClose } from "react-icons/fa";
+  import React, { Component } from "react";
+  import "./Main.css";
+  import { FaEdit, FaPlus, FaWindowClose } from "react-icons/fa";
 
-export default class Main extends Component {
-  state = {
-    newTask: "",
-    tasks: [],
-  };
+  export default class Main extends Component {
+    state = {
+      newTask: "",
+      tasks: [],
+      index: -1,
+    };
 
-  handleChange = (event) => {
-    this.setState({ newTask: event.target.value });
-  };
+    handleChange = (event) => {
+      this.setState({ newTask: event.target.value });
+    };
 
-  handleEdit = (event) => {
-    const { tasks } = this.state;
-    const taskToEdit = event.target.parentNode.parentNode.textContent;
-    const newTasks = tasks.filter((task) => task !== taskToEdit);
-    
-    this.setState({ tasks: newTasks });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { tasks, newTask } = this.state;
-
-    const trimmedTask = newTask.trim();
-
-    if (trimmedTask === "" || tasks.indexOf(trimmedTask) !== -1) {
-      return;
+    handleEdit = (index) => {
+      const { tasks } = this.state;
+      this.setState({
+        index: index,
+        newTask: tasks[index],
+      });
     }
 
-    const createNewTasks = [...tasks, trimmedTask];
+    handleSubmit = (event) => {
+      event.preventDefault();
+      const { tasks, newTask, index } = this.state;
 
-    this.setState({
-      tasks: createNewTasks,
-      newTask: "", // Limpa o campo de entrada
-    });
-  }
+      const trimmedTask = newTask.trim();
 
-  handleDelete = (event) => {
-    const { tasks } = this.state;
-    const taskToDelete = event.target.parentNode.parentNode.textContent;
-    const newTasks = tasks.filter((task) => task !== taskToDelete);
+      if (trimmedTask === "" || tasks.indexOf(trimmedTask) !== -1) {
+        return;
+      }
 
-    this.setState({ tasks: newTasks });
-  };
+      const newTasks = [...tasks];
+      
 
-  render() {
-    const { newTask, tasks } = this.state;
+      if(index === -1) {
+        this.setState({
+          tasks: [...tasks, trimmedTask],
+          newTask: "", // Limpa o campo de entrada após a submissão do formulário
+        });
+      }else{
+        
+        newTasks[index] = trimmedTask;
+        this.setState({
+          tasks: [...newTasks],
+          newTask: "",
+          index: -1  
+        });
+        
+      }
+    }
 
-    return (
-      <>
-        <div className="main">
-          <h1>Todo List</h1>
-          <form onSubmit={this.handleSubmit} className="form" action="#">
-            
-            <input
-              onChange={this.handleChange}
-              type="text"
-              value={newTask}
-            />
-            <button type="submit">
-              <FaPlus />
-            </button>
-          </form>
-          <ul className="tasks">
-            {tasks.map((task, index) => (
-              <li key={task}>
-                {task}
-                <span className="buttons">
-                  <FaEdit className="button_edit" />
+    handleDelete = (event) => {
+      const { tasks } = this.state;
+      const taskToDelete = event.target.parentNode.parentNode.textContent;
+      const newTasks = tasks.filter((task) => task !== taskToDelete);
+
+      this.setState({ tasks: newTasks });
+    };
+
+    render() {
+      const { newTask, tasks } = this.state;
+
+      return (
+        <>
+          <div className="main">
+            <h1>Todo List</h1>
+            <form onSubmit={this.handleSubmit} className="form" action="#">
+              
+              <input
+                onChange={this.handleChange}
+                type="text"
+                value={newTask}
+              />
+              <button type="submit">
+                <FaPlus />
+              </button>
+            </form>
+            <ul className="tasks">
+              {tasks.map((task, index) => (
+                <li key={task}>
+                  {task}
+                  <span className="buttons">
+                  <FaEdit className="button_edit" onClick={() => this.handleEdit(index)} />
                   <FaWindowClose className="button_delete" onClick={(event) => this.handleDelete(event, index) } />
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </>
-    );
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      );
+    }
   }
-}
